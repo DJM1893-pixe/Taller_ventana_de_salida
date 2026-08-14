3e6d0bbf6c27d2efbbcdcdb2bb8f55d56161700c
Daniel Jimenez <daniel_j0118@hotmail.com>
2026-08-14 14:45:29 -0500

    test de cada ejercico para prueba

---
+++ prueba.js
@@ -0,0 +1,28 @@
+let numero1 = Number(prompt("Digite el primer número"));
+let numero2 = Number(prompt("Digite el segundo número"));
+
+let suma = numero1 + numero2;
+let resta = numero1 - numero2;
+let multiplicacion = numero1 * numero2;
+let division = numero1 / numero2;
+let modulo = numero1 % numero2;
+
+alert("Suma: " + suma);
+console.log("Suma: " + suma);
+document.writeln("<p>Suma: " + suma + "</p>");
+
+alert("Resta: " + resta);
+console.log("Resta: " + resta);
+document.writeln("<p>Resta: " + resta + "</p>");
+
+alert("Multiplicación: " + multiplicacion);
+console.log("Multiplicación: " + multiplicacion);
+document.writeln("<p>Multiplicación: " + multiplicacion + "</p>");
+
+alert("División: " + division);
+console.log("División: " + division);
+document.writeln("<p>División: " + division + "</p>");
+
+alert("Módulo: " + modulo);
+console.log("Módulo: " + modulo);
+document.writeln("<p>Módulo: " + modulo + "</p>");
